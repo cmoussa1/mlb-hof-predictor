dcc204bc036711baa77f5a5e20cab32e02edda85
cmoussa1 <moussa1@llnl.gov>
2025-05-15 23:07:29 -0700

    visuals: add wRC+ graph

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -240,3 +240,65 @@ window.addEventListener('DOMContentLoaded', async () => {
         console.error('Error loading avg_woba_by_position.csv:', error);
     }
 }
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await renderAvgWRCPlusChart();
+});
+
+    async function renderAvgWRCPlusChart() {
+    try {
+        const response = await fetch('general_data/avg_wrcplus_by_position.csv');
+        const text = await response.text();
+        const [headerLine, dataLine] = text.trim().split('\n');
+
+        const labels = headerLine.split(',');
+        const data = dataLine.split(',').map(val => parseFloat(val));
+
+        const ctx = document.getElementById('avgWRCPlusByPositionChart').getContext('2d');
+        new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels,
+            datasets: [{
+            label: 'Average wRC+',
+            data,
+            backgroundColor: '#e03a2b',
+            borderColor: '#e03a2b',
+            borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+            y: {
+                beginAtZero: false,
+                min: 100,
+                max: 145,
+                ticks: {
+                    stepSize: 5
+                },
+                title: { display: true, text: 'wRC+' }
+            },
+            x: {
+                title: { display: true, text: 'positional group' }
+            }
+            },
+            plugins: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Average wRC+ by Position'
+            },
+            tooltip: {
+                callbacks: {
+                label: context => `${context.parsed.y.toFixed(3)}`
+                }
+            }
+            }
+        }
+        });
+
+    } catch (error) {
+        console.error('Error loading avg_wrcplus_by_position.csv:', error);
+    }
+}
