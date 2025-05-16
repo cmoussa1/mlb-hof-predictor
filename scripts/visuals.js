ca3688b0b45d70199fe7f98bf7e25c69f50c1384
cmoussa1 <moussa1@llnl.gov>
2025-05-15 22:19:38 -0700

    visuals: add average oWAR graph

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -54,3 +54,65 @@ window.addEventListener('DOMContentLoaded', async () => {
         console.error('Error loading avg_hof_grade_by_position.csv:', error);
     }
 }
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await renderAvgoWARChart();
+});
+
+    async function renderAvgoWARChart() {
+    try {
+        const response = await fetch('general_data/avg_owar_by_position.csv');
+        const text = await response.text();
+        const [headerLine, dataLine] = text.trim().split('\n');
+
+        const labels = headerLine.split(',');
+        const data = dataLine.split(',').map(val => parseFloat(val));
+
+        const ctx = document.getElementById('avgOWARByPositionChart').getContext('2d');
+        new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels,
+            datasets: [{
+            label: 'Average oWAR',
+            data,
+            backgroundColor: '#2bbfe0',
+            borderColor: '#2bbfe0',
+            borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+            y: {
+                beginAtZero: false,
+                min: 40,
+                max: 65,
+                ticks: {
+                    stepSize: 5
+                },
+                title: { display: true, text: 'average grade' }
+            },
+            x: {
+                title: { display: true, text: 'positional group' }
+            }
+            },
+            plugins: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Average oWAR by Position'
+            },
+            tooltip: {
+                callbacks: {
+                label: context => `Grade: ${context.parsed.y.toFixed(2)}`
+                }
+            }
+            }
+        }
+        });
+
+    } catch (error) {
+        console.error('Error loading avg_owar_by_position.csv:', error);
+    }
+}
